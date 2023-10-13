c5060db1b1ea883e348917b805c8184afe244c83
sajadgilga <majidstic@yahoo.com>
2023-10-13 14:48:39 +0330

    Add book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+from users.models import BaseModel
+
+User = get_user_model()
+
+
+class Book(BaseModel):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    text = models.TextField()
